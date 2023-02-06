135bcd3a7080344e883ed3ee4619c1ec8d142907
DanielQuilcue <danielquilcue18@gmail.com>
2023-02-05 21:21:40 -0500

    Project initiation

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,89 @@
+const Formulario = () => {
+  return (
+    <div className="md:w-1/2 lg:w-2/5">
+      <h1 className="font-black text-3xl text-center mb-10">
+        Siguimiento Pacientes
+      </h1>
+      <p className="text-lg mt-5 text-center">
+        Añade Pacientes y {""}
+        <span className="text-indigo-600 font-bold">Administrarlos</span>
+      </p>
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <div className="mb-5">
+          <label
+            htmlFor="mascota"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre Mascota
+          </label>
+          <input
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            type="text"
+            id="mascota"
+            placeholder="Nombre de la mascota"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="propietario"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre Propietario
+          </label>
+          <input
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            type="text"
+            id="propietario"
+            placeholder="Nombre del Propietario"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Email
+          </label>
+          <input
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            type="email"
+            id="email"
+            placeholder="Email de Contacto Propietario"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="alta"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Alta
+          </label>
+          <input
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            type="date"
+            id="alta"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="sintomas"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Sintomas
+          </label>
+          <textarea 
+            className="border-2 w-full p-2 mt-2 text-gray-700 font-bold"
+            id="sintomas"
+            placeholder="Describe los Sintomas"
+            />
+        </div>
+        <input type="submit"
+        className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+        value="Agregar Paciente" />
+        
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,10 @@
+
+const ListadoPacientes = () => {
+  return (
+    <div className="w-1/2 lg:wd-3/5">
+      <h2>pacientes</h2>
+    </div>
+  )
+}
+
+export default ListadoPacientes
